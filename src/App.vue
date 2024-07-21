ea6feba7779774f171539c696d3af5208f9cd9f3
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-07-21 11:46:59 -0500

    index action working

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <script>
-  export default {
-    data: function() {
-      return {
-        message: "Welcome to Vue.js!",
-      };
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      todos: [],
+    };
+  },
+  created: function () {
+    this.indexTodos();
+  },
+  methods: {
+    indexTodos: function () {
+      axios.get("http://localhost:5000/todos").then((response) => {
+        console.log("todos index", response);
+        this.todos = response.data;
+      });
     },
-    created: function() {},
-    methods: {},
-  }
+  },
+};
 </script>
 
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>All Todos</h1>
+    <div v-for="todo in todos" v-bind:key="todo.id">
+      <h2>{{ todo.title }}</h2>
+    </div>
   </div>
 </template>
+
+<style></style>
