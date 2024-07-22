4d90acc71e434f05ca27c2b6230563cc942c519b
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-07-22 07:20:55 -0500

    add method working

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ export default {
   data: function () {
     return {
       todos: [],
+      newTodoParams: {},
     };
   },
   created: function () {
@@ -16,12 +17,30 @@ export default {
         this.todos = response.data;
       });
     },
+    createTodo: function () {
+      axios
+        .post("http://localhost:5000/add", this.newTodoParams)
+        .then((response) => {
+          console.log("todo create", response);
+          this.todos.push(response.data);
+          this.newTodoParams = {};
+        })
+        .catch((error) => {
+          console.log("todo create error", error.response);
+        });
+    },
   },
 };
 </script>
 
 <template>
   <div class="home">
+    <h1>New Todo</h1>
+    <div>
+      Title:
+      <input type="text" v-model="newTodoParams.title" />
+      <button v-on:click="createTodo()">Create Todo</button>
+    </div>
     <h1>All Todos</h1>
     <div v-for="todo in todos" v-bind:key="todo.id">
       <h2>{{ todo.title }}</h2>
