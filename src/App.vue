a96fe2a931256a96ad0020d83bf242a3108c1a0e
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-07-27 14:52:19 -0500

    styling

--- src/App.vue
+++ src/App.vue
@@ -66,23 +66,46 @@ export default {
 </script>
 
 <template>
-  <div class="home w-screen h-screen bg-gradient-to-br from-blue-200 to-cyan-200">
+  <div class="p-6 home w-screen h-screen bg-gradient-to-br from-blue-200 to-cyan-200">
+    <div class="paper p-10">
+      <h1 class="text-center">Add Task</h1>
+      <div>
+        <input
+          type="text"
+          v-model="newTodoParams.title"
+          id="title"
+          class="m-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="study for test"
+          required
+        />
+        <button
+          v-on:click="createTodo()"
+          class="text-white bg-cyan-700 hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-cyan-600 dark:hover:bg-sky-700 dark:focus:ring-cyan-900"
+        >
+          Create
+        </button>
+      </div>
+    </div>
     <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
       <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Things to do:</span>
       {{ formattedDate }}
     </h1>
-    <!-- <div class="paper"></div>
-    <div class="paper"></div> -->
-    <h1>New Todo</h1>
-    <div>
-      Title:
-      <input type="text" v-model="newTodoParams.title" />
-      <button v-on:click="createTodo()">Create Todo</button>
-    </div>
-    <h1>All Todos</h1>
-    <div v-for="todo in todos" v-bind:key="todo.id">
-      <h2>{{ todo.title }}</h2>
-      <button v-on:click="showTodo(todo)">More Info</button>
+    <div class="grid md:grid-cols-3 gap-4">
+      <div
+        v-for="todo in todos"
+        v-bind:key="todo.id"
+        class="shadow-lg block rounded-lg bg-white p-6 text-surface shadow-secondary-1 m-4"
+      >
+        <h5 class="mb-2 text-xl font-medium leading-tight">{{ todo.title }}</h5>
+        <button
+          v-on:click="showTodo(todo)"
+          class="inline-block rounded bg-sky-300 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white hover:bg-sky-500"
+          data-twe-ripple-init
+          data-twe-ripple-color="light"
+        >
+          More Info
+        </button>
+      </div>
     </div>
     <dialog id="todo-details">
       <form method="dialog">
