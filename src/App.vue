caa2ce0f247a8c68f011a438d514f94fea31741d
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-07-28 09:42:11 -0500

    getting ready to separate update method in order to change text color (I know, it's a lot)

--- src/App.vue
+++ src/App.vue
@@ -114,13 +114,23 @@ export default {
         </button>
       </div>
     </div>
-    <dialog id="todo-details" class="w-[60%] min-w-[40%] p-6 border border-black">
+    <dialog id="todo-details" class="w-[60%] min-w-[40%] p-8 bg-fuchsia-100 rounded-2xl shadow-2xl">
       <form method="dialog">
-        <h1>Todo Info</h1>
-        <p>
-          Title:
-          <input type="text" v-model="editTodoParams.title" />
-        </p>
+        <h1
+          class="drop-shadow-2xl text-center mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"
+        >
+          <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Info</span>
+        </h1>
+        <div class="w-full">
+          <p class="text-xl font-semibold text-cyan-700">
+            Title:
+            <input
+              type="text"
+              v-model="editTodoParams.title"
+              class="placeholder-sky-500 text-fuchsia-200 mt-2 p-2 border border-gray-300 rounded"
+            />
+          </p>
+        </div>
         <div class="w-full flex justify-between">
           <button
             v-on:click="updateTodo(currentTodo)"
